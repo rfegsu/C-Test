ae4fa2c6cc3809e6867e1558d3106801a2bbff95
rfegsu <fergusmolloy@btinternet.com>
2018-04-22 14:20:42 +0100

    Add estimate pi program

---
+++ EstimatePi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EstimatePi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 200;
+            double total = 0;
+            double circle = 0;
+            double z = 0;
+            while(z<1E10)
+            {
+                double pi = 0;
+                for(int i=0; i<1E6; i++)
+                {
+                    Random rnd = new Random();
+                    int x = rnd.Next(-r, r);
+                    int y = rnd.Next(-r, r);
+                    double d = x*x + y*y;
+                    if(d < r*r)
+                    {
+                        circle++;
+                    }
+                    total++;
+                    pi = 4 * (circle/total);
+                }
+                Console.WriteLine(pi);
+                z++;
+            }
+        }
+    }
+}
