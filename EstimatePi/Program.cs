5b66b4beef0f04172f91f7d9b66654fe45438dd2
rfegsu <fergusmolloy@btinternet.com>
2018-04-22 15:00:06 +0100

    Added Other examples of basic programs finalised estimate pi

--- EstimatePi/Program.cs
+++ EstimatePi/Program.cs
@@ -9,26 +9,43 @@ namespace EstimatePi
             int r = 200;
             double total = 0;
             double circle = 0;
-            double z = 0;
-            while(z<1E10)
+            Random rnd = new Random();
+            double d;
+            int x;
+            int y;
+            double pi = 0;
+            double recDiff=0;
+            double diff = 0;
+            double recPi = 0;
+
+            int user_input;
+            Console.WriteLine("How many times would you like to loop?");
+            user_input = Convert.ToInt32(Console.ReadLine());
+            for(int z=0; z<user_input; z++)
             {
-                double pi = 0;
-                for(int i=0; i<1E6; i++)
+                for(int i=0; i<10000; i++)
                 {
-                    Random rnd = new Random();
-                    int x = rnd.Next(-r, r);
-                    int y = rnd.Next(-r, r);
-                    double d = x*x + y*y;
+                    x = rnd.Next(-r, r);
+                    y = rnd.Next(-r, r);
+                    d = x*x + y*y;
                     if(d < r*r)
                     {
                         circle++;
                     }
                     total++;
                     pi = 4 * (circle/total);
+                    recDiff = Math.Abs(Math.PI - recPi);
+                    diff = Math.Abs(Math.PI - pi);
+
+                    if(diff < recDiff)
+                    {
+                        recDiff = diff;
+                        recPi = pi;
+                        Console.WriteLine("New record: {0}", recPi);
+                    }
                 }
-                Console.WriteLine(pi);
-                z++;
             }
+            Console.WriteLine("Actual value of PI: {0}", Math.PI);
         }
     }
 }
