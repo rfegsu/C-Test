d5af421fec5ae45894ffa6961827499d4a6e094f
rfegsu <fergusmolloy@btinternet.com>
2018-04-23 15:33:09 +0100

    see Hello world for how to import namespaces

---
+++ HelloWorldApp/test.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace test
+{
+    class Test
+    {
+        private string str;
+        public Test(string text)
+        {
+            str = text;
+        }
+        public string getString()
+        {
+            return str;
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        public string str;
+        static void Main(string[] args)
+        {
+            str = "hello world"
+        }
+    }
+}
